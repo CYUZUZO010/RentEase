50e41f937fe43021f11f83f2879777efd46ae551
CYUZUZO010 <sandcyuzuzo@gmail.com>
2025-10-26 00:54:16 +0200

    Update property-card.tsx

--- src/components/property-card.tsx
+++ src/components/property-card.tsx
@@ -17,6 +17,7 @@ interface PropertyCardProps {
     available: boolean
   }
 }
+//all the property cards will be displayed with this ui .
 
 export function PropertyCard({ property }: PropertyCardProps) {
   return (
